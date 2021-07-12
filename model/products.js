91dda2d541b3afe03f530cf2d5301e6bfc9ec1e6
telvinpaul1 <telvinpaul33@gmail.com>
2021-07-12 16:48:32 +0100

    week6 weekly task ecomerce-app

---
+++ model/products.js
@@ -0,0 +1,57 @@
+const products_list = [
+    {
+        id: 001,
+        name:"watch",
+        decription: "smart watch waterproof",
+        image:"https://www.jumia.com.ng/generic-smart-watch-waterproof-blood-pressure-movement-heart-rate-watch-71996641.html",
+        price:1103500
+    },
+
+    {
+        id: 002,
+        name:"chair",
+        decription: "Conference Room Sofa Chai",
+        image:"https://www.jumia.com.ng/generic-conference-room-sofa-chair.-multicolor.-delivery-only-in-lagos-21911227.html",
+        price:350000
+    },
+
+    {
+        id: 003,
+        name:"Toyota Highlande Jeep",
+        decription: "2015 Toyota Highlander",
+        image:"https://buy.cars45.com/toyota-highlander-2015-automatic-ng-825874",
+        price:12330000
+    },
+    {
+        id: 004,
+        name:"truck",
+        decription: "Isuzu NQR 2020",
+        image:"https://buy.cars45.com/isuzu-nqr-2020-manual-ng-540684",
+        price:16500000
+    },
+    {
+        id: 005,
+        name:"jeep",
+        decription: "2019 Jeep Wrangler",
+        image:"https://buy.cars45.com/jeep-wrangler-2019-automatic-sahara-296hp-ng-536253",
+        price:32000000
+    },
+    {
+        id: 006,
+        name:"automatic car",
+        decription: "2019 Mercedes-Benz",
+        image:"https://buy.cars45.com/cars?product_id=31970",
+        price:20000000
+    },
+    {
+        id: 007,
+        name:"lexu",
+        decription: "2016 Lexus GX460",
+        image:"https://buy.cars45.com/cars?product_id=31971",
+        price:18000000
+    },
+
+]
+module.exports = products_list;
+
+
